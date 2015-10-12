244e045db6c6dc8719f6138fa330025d3c21fa9f
ömer demircan <demircanomer91@gmail.com>
2015-10-12 21:51:13 +0300

    indent

--- linkedList/linked.js
+++ linkedList/linked.js
@@ -1,52 +1,52 @@
 var Node = require('./node.js');
 
 function LinkedList(){
-	this.head = new Node("head");
-	this.current = this.head;
-	this.insert = insert;
-	this.remove = remove;
-	this.find = find;
-	this.display = display;
+    this.head = new Node("head");
+    this.current = this.head;
+    this.insert = insert;
+    this.remove = remove;
+    this.find = find;
+    this.display = display;
 }
 
 function insert(newElement){
-	var newNode = new Node(newElement);
-	this.current.next = newNode;
-	newNode.next = null;
-	this.current = newNode;
+    var newNode = new Node(newElement);
+    this.current.next = newNode;
+    newNode.next = null;
+    this.current = newNode;
 };
 
 function find(item){
-	var currNode = this.head;
-	while(currNode.element != item){
-		currNode = currNode.next;
-	}
-	return currNode;
+    var currNode = this.head;
+    while(currNode.element != item){
+        currNode = currNode.next;
+    }
+    return currNode;
 };
 
 function display(){
-	var currNode = this.head;
-	do{
-		console.log(currNode.next.element);
-		currNode = currNode.next;
-	}while(currNode.next!=null);
+    var currNode = this.head;
+    do{
+        console.log(currNode.next.element);
+        currNode = currNode.next;
+    }while(currNode.next!=null);
 };
 
 function remove(node){
-	var self = this;
-	
-	var prev = prevNode(node);
+    var self = this;
+    
+    var prev = prevNode(node);
 
-	var willDelete = this.find(node);
-	prev.next = willDelete.next;
-	
-	function prevNode(node){
-		var currNode = self.head;
-		while(currNode.next.element != node){
-			currNode = currNode.next;
-		}
-		return currNode;
-	}
+    var willDelete = this.find(node);
+    prev.next = willDelete.next;
+    
+    function prevNode(node){
+        var currNode = self.head;
+        while(currNode.next.element != node){
+            currNode = currNode.next;
+        }
+        return currNode;
+    }
 
 };
 
